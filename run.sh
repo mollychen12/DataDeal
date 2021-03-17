9f4faef2ee9fc4965c11b9d6826247c36b5653af
mollychen12 <32167268+mollychen12@users.noreply.github.com>
2021-03-17 11:47:49 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#get the info of the processor
+pod=hostname
+pid=$$
+echo "The pod:${pod}"
+echo "The pid:${pid}"
+
+#list the array
+x_coord=(0.1 0.2 0.3 0.4)
+y_coord=(0.1 0.2 0.3 0.4)
+angle=(5 10 15 20 25)
+
+#trasverse the angle and run
+for index in 0 1 2 3 4;do
+
+done
+
+
